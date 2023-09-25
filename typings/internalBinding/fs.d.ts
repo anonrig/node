78557c134f253f4d2123ed722b2db35a3121b629
CanadaHonk <honk@goose.icu>
2023-09-25 03:45:23 +0100

    fs: improve error performance for `rmdirSync`

--- typings/internalBinding/fs.d.ts
+++ typings/internalBinding/fs.d.ts
@@ -185,7 +185,7 @@ declare namespace InternalFSBinding {
   function rename(oldPath: string, newPath: string, usePromises: typeof kUsePromises): Promise<void>;
 
   function rmdir(path: string, req: FSReqCallback): void;
-  function rmdir(path: string, req: undefined, ctx: FSSyncContext): void;
+  function rmdir(path: string): void;
   function rmdir(path: string, usePromises: typeof kUsePromises): Promise<void>;
 
   function stat(path: StringOrBuffer, useBigint: boolean, req: FSReqCallback<Float64Array | BigUint64Array>): void;
