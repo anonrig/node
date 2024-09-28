a558b0f3837e09cd58fb49ec7f77a3699dcac0e5
Yagiz Nizipli <yagiz@nizipli.com>
2024-09-28 14:27:37 -0400

    src: remove windows server 2012 support

--- src/node_main.cc
+++ src/node_main.cc
@@ -27,25 +27,11 @@
 #include <VersionHelpers.h>
 #include <WinError.h>
 
-#define SKIP_CHECK_VAR "NODE_SKIP_PLATFORM_CHECK"
-#define SKIP_CHECK_VALUE "1"
-#define SKIP_CHECK_STRLEN (sizeof(SKIP_CHECK_VALUE) - 1)
-
 int wmain(int argc, wchar_t* wargv[]) {
-  // Windows Server 2012 (not R2) is supported until 10/10/2023, so we allow it
-  // to run in the experimental support tier.
-  char buf[SKIP_CHECK_STRLEN + 1];
-  if (!IsWindows10OrGreater() &&
-      (GetEnvironmentVariableA(SKIP_CHECK_VAR, buf, sizeof(buf)) !=
-           SKIP_CHECK_STRLEN ||
-       strncmp(buf, SKIP_CHECK_VALUE, SKIP_CHECK_STRLEN) != 0)) {
+  if (!IsWindows10OrGreater()) {
     fprintf(stderr,
             "Node.js is only supported on Windows 10, Windows "
-            "Server 2016, or higher.\n"
-            "Setting the " SKIP_CHECK_VAR " environment variable "
-            "to 1 skips this\ncheck, but Node.js might not execute "
-            "correctly. Any issues encountered on\nunsupported "
-            "platforms will not be fixed.");
+            "Server 2016, or higher.");
     exit(ERROR_EXE_MACHINE_TYPE_MISMATCH);
   }
 
